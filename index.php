710e600b8c469ac7ef541444ca63ddd8a23b5bb2
Yasmin <khairinissakarin67@gmail.com>
2025-06-09 16:48:10 +0700

    commit index

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Index File - Entry Point dengan Routing
+ * 
+ * File utama yang menggunakan sistem routing sederhana
+ * Pendekatan sederhana untuk mahasiswa semester 1
+ */
+
+// Start session untuk authentication
+session_start();
+
+// Include file routing
+require_once 'routes/web.php';
+
+?> 
