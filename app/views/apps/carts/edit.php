41bc8bcc65982e892aac85a714acbc739879aa44
Yasmin <anggraenidian531@gmail.com>
2025-06-12 11:29:09 +0700

    Hapus folder app, terdapat kesalahan

--- app/views/apps/carts/edit.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirect to cart index since edit is done inline in the cart index page
-header('Location: ' . url('carts'));
-exit;
-?>
--- app/views/apps/carts/show.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Redirect to cart index since show individual cart item is not needed
-header('Location: ' . url('carts'));
-exit;
-?>
